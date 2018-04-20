833c37fb5f7a80b541b5e9b073aa5bedea8dc662
Single Long <623962644@qq.com>
2018-04-20 11:22:52 +0800

    脑机接口通信的准备工作
    初步规划好窗口布局

---
+++ server/server/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.IO;
+using System.Windows.Threading;
+
+namespace server
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            DispatcherTimer dateTimer = new DispatcherTimer();//显示当前时间线程
+            dateTimer.Tick += new EventHandler(dateTimer_Tick);
+            dateTimer.Interval = new TimeSpan(0, 0, 0, 1, 0);
+            dateTimer.Start();
+        }
+
+        private void dateTimer_Tick(object sender, EventArgs e)//取当前时间的委托
+        {
+            string timeDateString = "";
+            DateTime now = DateTime.Now;
+            timeDateString = string.Format("{0}年{1}月{2}日 {3}:{4}:{5}",
+                now.Year,
+                now.Month.ToString("00"),
+                now.Day.ToString("00"),
+                now.Hour.ToString("00"),
+                now.Minute.ToString("00"),
+                now.Second.ToString("00"));
+
+            timeDateTextBlock.Text = timeDateString;
+        }
+
+        private void Run_Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Shut_Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
