850132b5f29d8a06b41ef87b6e9fba1db68b0cda
Single Long <623962644@qq.com>
2018-04-24 19:57:01 +0800

    基本实现C-S框架的通信功能
    仍有不少完善的地方：如客户端关闭后，服务器无法再次启动等问题

---
+++ ExoSocket/Client/MainWindow.xaml.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace Client
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public delegate void showData(string msg); //通信窗口输出相关
+        TcpClient client;
+        NetworkStream sendStream;
+        private const int bufferSize = 8000;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Switch_Button_Click(object sender, RoutedEventArgs e) //请求连接
+        {
+            if (IPAdressTextBox.Text.Trim() == string.Empty)
+            {
+                ComWinTextBox.Dispatcher.Invoke(new showData(ComWinTextBox.AppendText), "请填入服务器IP地址\n");
+                return;
+            }
+            if (PortTextBox.Text.Trim() == string.Empty)
+            {
+                ComWinTextBox.Dispatcher.Invoke(new showData(ComWinTextBox.AppendText), "请填入服务器端口号\n");
+                return;
+            }
+
+            IPAddress ip = IPAddress.Parse(IPAdressTextBox.Text);
+            client = new TcpClient();
+            ComWinTextBox.AppendText("开始连接服务端....\n");
+            client.Connect(ip, int.Parse(PortTextBox.Text));
+            ComWinTextBox.AppendText("已经连接服务端\n");
+            statusInfoTextBlock.Text = "已与服务器建立连接";
+
+            sendStream = client.GetStream();
+            Thread thread = new Thread(ListenerServer);
+            thread.Start();
+        }
+
+        private void ListenerServer()
+        {
+            do
+            {
+                try
+                {
+                    int readSize;
+                    byte[] buffer = new byte[bufferSize];
+                    lock (sendStream)
+                    {
+                        readSize = sendStream.Read(buffer, 0, bufferSize);
+                    }
+                    if (readSize == 0)
+                        return;
+                    ComWinTextBox.Dispatcher.Invoke(new showData(ComWinTextBox.AppendText), "从服务端发来信息：" + Encoding.Default.GetString(buffer, 0, readSize) + "\n");
+
+                }
+                catch
+                {
+                    ComWinTextBox.Dispatcher.Invoke(new showData(ComWinTextBox.AppendText), "报错\n");
+                }
+
+            } while (true);
+        }
+
+        private void Send_Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (client != null)
+            {
+                //要发送的信息
+                if (MessageTextBox.Text.Trim() == string.Empty)
+                    return;
+                string msg = MessageTextBox.Text.Trim();
+
+                byte[] buffer = Encoding.Default.GetBytes(msg);
+                //lock (sendStream)
+                //{
+                sendStream.Write(buffer, 0, buffer.Length);
+                //}
+                ComWinTextBox.AppendText("发送给服务端的数据：" + msg + "\n");
+                MessageTextBox.Text = string.Empty;
+            }
+        }
+
+        private void ComWinTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            ComWinTextBox.ScrollToEnd(); //当通信窗口内容有变化时保持滚动条在最下面
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+    }
+}
--- ExoSocket/Server/MainWindow.xaml.cs
+++ ExoSocket/Server/MainWindow.xaml.cs
@@ -12,10 +12,10 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using System.Windows.Threading;
-using System.Net;
+
 using System.Net.Sockets;
 using System.Threading;
+using System.Net;
 
 namespace Server
 {
@@ -24,56 +24,105 @@ namespace Server
     /// </summary>
     public partial class MainWindow : Window
     {
-        private ServerFunc server = new ServerFunc();
+        public delegate void showData(string msg); //通信窗口输出相关
+        private TcpListener server;
+        private TcpClient client;
+        private const int bufferSiize = 8000;
 
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        private void Window_Loaded(object sender, RoutedEventArgs e)
+        struct IpAndPort
         {
-            ControlWriter writer = new ControlWriter(ComWinTextBox); //将TextBox转化成Console
-
-            DispatcherTimer dateTimer = new DispatcherTimer();//显示当前时间线程
-            dateTimer.Tick += new EventHandler(dateTimer_Tick);
-            dateTimer.Interval = new TimeSpan(0, 0, 0, 1, 0);
-            dateTimer.Start();
+            public string Ip;
+            public string Port;
         }
 
-        private void dateTimer_Tick(object sender, EventArgs e)//取当前时间的委托
+        private void Switch_Button_Click(object sender, RoutedEventArgs e) //启动服务器
         {
-            string timeDateString = "";
-            DateTime now = DateTime.Now;
-            timeDateString = string.Format("{0}年{1}月{2}日 {3}:{4}:{5}",
-                now.Year,
-                now.Month.ToString("00"),
-                now.Day.ToString("00"),
-                now.Hour.ToString("00"),
-                now.Minute.ToString("00"),
-                now.Second.ToString("00"));
+            if (IPAdressTextBox.Text.Trim() == string.Empty)
+            {
+                ComWinTextBox.Dispatcher.Invoke(new showData(ComWinTextBox.AppendText), "请填入服务器IP地址\n");
+                return;
+            }
+            if (PortTextBox.Text.Trim() == string.Empty)
+            {
+                ComWinTextBox.Dispatcher.Invoke(new showData(ComWinTextBox.AppendText), "请填入服务器端口号\n");
+                return;
+            }
 
-            timeDateTextBlock.Text = timeDateString;
+            Thread thread = new Thread(reciveAndListener);
+            IpAndPort ipAndport = new IpAndPort();
+            ipAndport.Ip = IPAdressTextBox.Text;
+            ipAndport.Port = PortTextBox.Text;
+
+            thread.Start((object)ipAndport);
+            
+            ComWinTextBox.Dispatcher.Invoke(new showData(ComWinTextBox.AppendText), "服务器 " + ipAndport.Ip + " : " + ipAndport.Port + " 已开启监听\n");
+            statusInfoTextBlock.Text = "服务器已启动";
         }
 
-        private void Switch_Button_Click(object sender, RoutedEventArgs e)
+        private void reciveAndListener(object ipAndPort)
         {
-            server.Start(IPAdressTextBox, PortTextBox);
-            Console.WriteLine("服务器 " + IPAdressTextBox.Text.ToString() + ":" + PortTextBox.Text + " 已开启");
-            SwitchOff_Button.IsEnabled = true;
-            Switch_Button.IsEnabled = false;         
+            IpAndPort ipAndport = (IpAndPort)ipAndPort;
+
+            IPAddress ip = IPAddress.Parse(ipAndport.Ip);
+            server = new TcpListener(ip, int.Parse(ipAndport.Port));
+            server.Start();//启动监听
+
+            client = server.AcceptTcpClient();
+            ComWinTextBox.Dispatcher.Invoke(new showData(ComWinTextBox.AppendText), "有客户端请求连接，连接已建立\n");
+            //AcceptTcpClient 是同步方法，会阻塞进程，得到连接对象后才会执行这一步 
+
+            //获得流
+            NetworkStream reciveStream = client.GetStream();
+            #region
+            do
+            {
+                byte[] buffer = new byte[bufferSiize];
+                int msgSize;
+                try
+                {
+                    lock (reciveStream)
+                    {
+                        msgSize = reciveStream.Read(buffer, 0, bufferSiize);
+                    }
+                    if (msgSize == 0)
+                        return;
+                    string msg = Encoding.Default.GetString(buffer, 0, bufferSiize);
+                    ComWinTextBox.Dispatcher.Invoke(new showData(ComWinTextBox.AppendText), "从客户端发来信息：" + Encoding.Default.GetString(buffer, 0, msgSize) + "\n");
+                }
+                catch
+                {
+                    ComWinTextBox.Dispatcher.Invoke(new showData(ComWinTextBox.AppendText), "出现异常：连接被迫关闭\n");
+                    break;
+                }
+            } while (true);
+            #endregion
+        }
+
+
+        private void Send_Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (MessageTextBox.Text.Trim() != string.Empty)
+            {
+                NetworkStream sendStream = client.GetStream();
+                byte[] buffer = Encoding.Default.GetBytes(MessageTextBox.Text.Trim());
+                sendStream.Write(buffer, 0, buffer.Length);
+                MessageTextBox.Text = string.Empty;
+            }
         }
 
         private void ComWinTextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
-            ComWinTextBox.ScrollToEnd(); //当通信窗口内容变化时滚动条定位在最下面
+            ComWinTextBox.ScrollToEnd(); //当通信窗口内容有变化时保持滚动条在最下面
         }
 
-        private void SwitchOff_Button_Click(object sender, RoutedEventArgs e)
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
         {
             server.Stop();
-            SwitchOff_Button.IsEnabled = false;
-            Switch_Button.IsEnabled = true;
         }
     }
 }
